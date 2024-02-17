4da71bfa5da9a5fb96390324318488e20ab169f1
MridulY <mridulyadav3173@gmail.com>
2024-02-17 21:05:26 +0530

    Updated psql url

--- db.ts
+++ db.ts
@@ -1,12 +1,12 @@
 import { Pool } from 'pg';
 import 'dotenv/config';
+dotenv.config();
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: parseInt(process.env.DB_PORT || '5432', 10),
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false 
+    }
 });
 
 
